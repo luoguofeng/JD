34bd9844e8d044b6c9a7a45b223f8229f389d632
Lgf <lgf@qq.com>
2019-10-02 18:57:21 +0800

    众筹

--- app/js/router/index.js
+++ app/js/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Home from '../home/index.vue'
 import Money from '../money/index.vue'
 import Ious from '../ious/index.vue'
+import Raise from '../raise/index.vue'
 import '../../css/reset.scss'
 Vue.use(Router)
 
@@ -22,6 +23,11 @@ export default new Router({
             path: '/ious',
             name: 'ious',
             component: Ious
+        },
+        {
+            path: '/raise',
+            name: 'raise',
+            component: Raise
         }
     ]
 })
